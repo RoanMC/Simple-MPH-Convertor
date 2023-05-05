1d8b9e852ee9ea69a0e500735b3048cf3eb1ec84
RoanMC <132460249+RoanMC@users.noreply.github.com>
2023-05-04 23:20:28 -0400

    Main

---
+++ Test.java
@@ -0,0 +1,12 @@
+public class Test {
+    public static void main(String[] args){
+
+                        // MPH to m/s Convertor
+            double miles = 1609.34 * 30; // converts miles into meters times the number of miles traveled
+            int hour = 3600; // measured in seconds
+            double result = miles / hour ; // Result. Which is Meters per second
+
+        System.out.println(result);
+
+    }
+}
